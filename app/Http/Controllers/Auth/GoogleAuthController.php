237fbc857967bc4f1ae2e38cfc5325bf8de681b0
JohnOtilla09 <otillajohn09@gmail.com>
2023-08-20 17:19:57 +0800

    GoogleAuthController | create google controller for google authentication

---
+++ app/Http/Controllers/Auth/GoogleAuthController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Exception;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Socialite\Facades\Socialite;
+
+class GoogleAuthController extends Controller
+{
+
+    const provider = 'google';
+
+    public function registerRedirect()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function registerCallback()
+    {
+        $SocialUser = Socialite::driver('google')->stateless()->user();
+        // dd($SocialUser);
+
+        return response($SocialUser, 201);
+        
+        // $userExisted = User::where('email', $SocialUser->email)->first();
+
+        // if (!$userExisted) {
+        //     $user = User::updateOrCreate([
+        //         'id' => $SocialUser->id,
+        //     ], [
+        //         'first_name' => $SocialUser->name,
+        //         'last_name' => $SocialUser->nickname,
+        //         'email' => $SocialUser->email,
+        //         'profile_picture_img' => $SocialUser->avatar,
+        //         // 'user_type_id' => $user_type_id
+        //     ]);
+
+        //     $token = $user->createToken('myapptoken')->plainTextToken;
+
+        //     $response = [
+        //         'user' => $user,
+        //         'token' => $token
+        //     ];
+
+        //     return response($response, 201);
+
+        // }
+
+        // $response = $userExisted;
+
+        // return response($response, 201);
+    }
+}
