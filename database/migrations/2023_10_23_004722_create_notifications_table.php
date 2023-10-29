50c6faaec8a09eb80a97c3a1e50475d5fc0fca1b
Ted30 <teddymarcenaje@gmail.com>
2023-10-29 15:36:25 +0800

    added notification routes,model and controller

---
+++ database/migrations/2023_10_23_004722_create_notifications_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('message');
+            $table->text('redirect_url');
+            $table->unsignedBigInteger('user_id');
+            $table->tinyInteger('is_read');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notifications');
+    }
+};
